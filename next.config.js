d3e44d787baf11e67a8f476c335d0b32f8e58580
cx33 <lne128gene980@163.com>
2023-07-14 10:45:16 +0800

    chore: update config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const nextConfig = {
     domains: [
       'images.unsplash.com',
       'plus.unsplash.com',
-    ]
-  }
+    ],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
